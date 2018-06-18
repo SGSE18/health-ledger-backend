b72df1d2b574cb6809566cfc8e1d9f4e847f14e5
Unknown <kschima@posteo.de>
2018-06-18 17:57:37 +0200

    Add redeem treatment

--- treatment/index.js
+++ treatment/index.js
@@ -14,9 +14,24 @@ router.post('/', async (req, res) => {
     if(!req.body.publicKey)
       throw new Error("No publicKey given");
     if(!req.body.treatment)
-      throw new Error("No request given")
+      throw new Error("No treatment given")
 
     await req.client.postTreatment(req.body.publicKey, req.body.treatment);
+    res.send();
+  } catch(err){
+    console.log(err)
+    res.status(500).send(err);
+  }
+});
+
+router.post('/redeem', async (req, res) => {
+  try {
+    if(!req.body.publicKey)
+      throw new Error("No publicKey given");
+    if(!req.body.treatmentId)
+      throw new Error("No treatmentId given")
+
+    await req.client.redeemTreatment(req.body.publicKey, req.body.treatmentId);
     res.send();
   } catch(err){
     console.log(err)
